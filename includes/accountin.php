592df47bf5a7dafdb29a3325699742d63972d552
Mosaddeq <36484786+Mosaddeq@users.noreply.github.com>
2018-03-30 23:05:05 +0600

    Create accountin.php

---
+++ includes/accountin.php
@@ -0,0 +1,20 @@
+</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td width="190" valign="top">
+            <b>&nbsp;Account</b><hr />
+            <ul>
+                <li><a href="#">Dashboard</a></li>
+				<li><a href="examIns.php">Exan Paper</a></li>
+                <li><a href="upload.php">Upload Files</a></li>
+				<li><a href="downloadFileIns.php">Download File</a></li>
+                <li><a href="profileIns.php">View Profile</a></li>
+                <li><a href="pictureIns.php">Change Profile Picture</a></li>
+                <li><a href="changeIns.php">Change Password</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </td>
+        <td valign="top">
