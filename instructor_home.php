248764ad71cda0fcbb4148970a049ccc2c15764a
Mosaddeq <36484786+Mosaddeq@users.noreply.github.com>
2018-03-31 04:23:08 +0600

    Update instructor_home.php

--- instructor_home.php
+++ instructor_home.php
@@ -1,15 +1,35 @@
 <?php 
-	session_start();
-	if(isset($_SESSION["email"]) ){
-		require_once "includes/headerin.php";
-		require_once "includes/accountin.php";
+	require_once ("includes/headerin.php");
+    require_once ("includes/accountin.php");
+    require ("includes/session.php");
 ?>
-<h1>Welcome Instructor Home Page!<br/>Email:&nbsp; <?=$_SESSION["email"]?></h1>
+<fieldset>
+    <legend><b>PROFILE View</b></legend>
+	<form>
+    <table cellpadding="0" cellspacing="0">
+    <tr>
+    <td>Name:</td>
+    <td>: <?php echo $login_session; ?></td>
+    <td rowspan="7" align="center">
+					<img width="128" src="image/user.png"/>
+                    <br/>
+                    <a href="pictureIns.php">Change</a>
+				</td>
+    </tr>
+    <td>Email:</td>
+    <td>: <?php echo $login_session1; ?></td>
+    </tr>
+    <td>Gender:</td>
+    <td>: <?php echo $login_session2; ?></td>
+    </tr>
+    <td>Designation:</td>
+    <td>: <?php echo $login_session3; ?></td>
+    </tr>
+    
+</table>
+		
+	</form>
+</fieldset>
 <?php 
 	require_once "includes/footerN.php";
-?>
-<?php
-	}else{
-		echo "Please login first";
-	}
 ?>
