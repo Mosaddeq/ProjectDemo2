cfe0a00888e8a00985666eee5a36451d1af6b7a7
Mosaddeq <36484786+Mosaddeq@users.noreply.github.com>
2018-03-30 23:01:37 +0600

    Create headerin.php

---
+++ includes/headerin.php
@@ -0,0 +1,13 @@
+<table width="100%" align="center" cellspacing="0" cellpadding="10" border="1">
+    <tr>
+        <td colspan="2" valign="middle" height="70">
+            <table width="100%">
+                <tr>
+                    <td>
+                        <a href="#">
+                            <img height="48" src="image/logo.png">
+                        </a>
+                    </td>
+                    <td align="right">
+                        Logged in as <a href="profileIns.php">Instructor</a>&nbsp;|
+                        <a href="logout.php">Logout</a>
