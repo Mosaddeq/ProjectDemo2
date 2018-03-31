887f1e451dcb4ebf73d08e242f4c1560090f684e
Mosaddeq <36484786+Mosaddeq@users.noreply.github.com>
2018-04-01 03:42:46 +0600

    Add files via upload

---
+++ sessionad.php
@@ -0,0 +1,30 @@
+<?php
+require("includes/dbconnect.php");
+
+session_start(); //start session
+
+$e_check = $_SESSION['email_check'];
+$pass_check=$_SESSION['login_password'];
+$sql="select * from reg where email = '$e_check' and password = '$pass_check' "; //checking sesisons from login.php under if state() $_SESSION['email_check']=$email;    
+                                                                              //initializing sessions
+//$_SESSION['login_password']=$pass;
+$sql="select * from admin where email = '$e_check' and password = '$pass_check' ";
+
+$result = mysqli_query($conn,$sql);
+$row = mysqli_fetch_assoc($result);
+
+
+
+$login_session = $row['name'];
+$login_session1 = $row['email'];
+$login_session2 = $row['gender'];
+$login_session3 = $row['utype'];
+$login_session4 = $row['cInfo'];
+
+
+if(!isset($login_session)){
+    mysqli_close($conn);
+
+}
+
+?>
